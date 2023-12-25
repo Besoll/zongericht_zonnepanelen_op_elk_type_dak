0d31c70eb48cb861e6cdb6b9ae1dc0e9abac0532
besoll <b.kavzharadze@gmail.com>
2023-12-25 21:39:12 +0100

    fix skeleton color, iamge, SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,15 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Zongericht',
   description: 'Zonnepanelen op elk type dak - Waar een dak is, schijnt de zon! Met Onze kennis transformeren we elk dak in een zonne-energiebron',
+  other: {
+    'theme-color': '#0d1117',
+    "color-scheme": "dark only",
+    "twitter:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+    "twitter:card": "summary_large_image",
+    "og:url": "zongericht.nl",
+    "og:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+    "og:type": "website",
+  }
 }
 
 export default function RootLayout({
