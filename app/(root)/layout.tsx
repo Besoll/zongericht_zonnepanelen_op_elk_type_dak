ae2fc33548737dd112c9279534daf2488c0c3b9e
besoll <b.kavzharadze@gmail.com>
2024-01-01 11:26:35 +0100

    testing without navbar and footer

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from '@/components/Navbar'
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-        <Navbar />
+        
         {children}
         
     </>
