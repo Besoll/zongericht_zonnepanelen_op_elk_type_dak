fa78aa4a69beaa4d63164b3067dc68c8429a4ffb
besoll <b.kavzharadze@gmail.com>
2024-01-04 01:37:36 +0100

    remove font, fix error

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const nextConfig = {
     images: {
         domains: ['cdn.sanity.io']
     },
-    reactStrictMode: true,
-    experimental: { optimizeCss: true }
+    optimizeFonts: true,
     
 }
 
