322c1488db1e47dc6ab99fc0980d4cf64e0844ec
besoll <b.kavzharadze@gmail.com>
2024-01-05 01:23:24 +0100

    trying to fix vercel error with PWA

--- next.config.js
+++ next.config.js
@@ -3,21 +3,16 @@ const withPWA = require('next-pwa');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['cdn.sanity.io'],
+        domains: ['cdn.sanity.io']
     },
     optimizeFonts: true,
-    // Other configurations...
-};
+}
+
 
 module.exports = withPWA({
     ...nextConfig,
     pwa: {
-        dest: 'public', // This will output the service worker to your public directory.
-        // You can add more configurations for PWA here.
-        // For example, to disable PWA in development mode:
-        // disable: process.env.NODE_ENV === 'development',
+        dest: 'public', // destination of the service worker
+        // other PWA-specific configurations...
     },
 });
-
-
-
