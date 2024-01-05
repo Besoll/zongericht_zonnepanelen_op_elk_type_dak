2b762ee52c6b6577849cc783e16a6184de857dd2
besoll <b.kavzharadze@gmail.com>
2024-01-05 01:03:07 +0100

    next-pwa installed

--- next.config.js
+++ next.config.js
@@ -1,12 +1,23 @@
+const withPWA = require('next-pwa');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['cdn.sanity.io']
+        domains: ['cdn.sanity.io'],
     },
     optimizeFonts: true,
-    
-}
+    // Other configurations...
+};
 
-module.exports = nextConfig
+module.exports = withPWA({
+    ...nextConfig,
+    pwa: {
+        dest: 'public', // This will output the service worker to your public directory.
+        // You can add more configurations for PWA here.
+        // For example, to disable PWA in development mode:
+        // disable: process.env.NODE_ENV === 'development',
+    },
+});
+
 
 
