7b330be93619513043a45e5f1314a50d5fae2a5e
besoll <b.kavzharadze@gmail.com>
2024-01-06 14:16:32 +0100

    change layout skeleton for home page

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -4,19 +4,30 @@ import { Skeleton } from "@/components/ui/skeleton"
 const loading = () => {
   return (
     <main className='flex-center paddings mx-auto w-full max-w-screen-2xl flex-col'>
-      <section className='nav-padding w-full'>
-        <Skeleton className="h-[274px] w-full rounded-lg bg-grey-100/60" />
-      </section>
 
-      <section className='mt-6 flex w-full flex-col sm:mt-20'>
-        <Skeleton className="h-10 w-56 bg-grey-100/60" />
-        <div className='mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start'>
-          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-grey-100/60" />
-          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-grey-100/60" />
-          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-grey-100/60" />
-        </div>
-      </section>
+        <section className='flex pt-[40px] md:pt-[98px] w-full flex-col text-base text-black'>
+              {/* top side of the hero */}
+              <div className='w-full flex flex-col lg:flex-row-reverse justify-between'>
+                {/* right side */}
+                <div className='lg:flex-col justify-start w-full lg:w-[45%]  '>
+                    <div className='flex flex-row justify-between pb-2'>
+                      <Skeleton className="object-cover min-h-[100px] h-auto w-[35%] rounded-xl pr-2" />
+                      <Skeleton className="object-cover min-h-[100px] h-auto w-full flex-col rounded-xl" />    
+                    </div>
+                    <Skeleton className="object-cover min-h-[100px] h-auto w-[65%] rounded-xl" />    
+                </div>
+                
+                {/* left side */}
+                <div className='flex-col justify-start w-full'>
+                  <Skeleton className="object-cover min-h-[250px] pr-5 rounded-xl" /> \
+                </div>
+              </div>
+        </section>
+             
     </main>
+
+
+
 
   )
 }
