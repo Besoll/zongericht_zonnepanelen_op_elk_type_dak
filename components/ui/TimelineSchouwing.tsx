b5a063d16cc6bd4749c5a8886cc61e0ba0663b01
besoll <b.kavzharadze@gmail.com>
2024-01-15 13:48:27 +0100

    change contrast on pulse animate and add robots.ts file for shcouwing page

--- components/ui/TimelineSchouwing.tsx
+++ components/ui/TimelineSchouwing.tsx
@@ -13,7 +13,7 @@ const TimelineSchouwing = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5 text-brand_original_secondary animate-pulse"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
                 </div>
                 <div className="timeline-start md:text-end mb-10">
-                <time className="font-mono italic text-brand_original_secondary animate-pulse">Stap 1</time>
+                <time className="font-mono italic animate-pulse">Stap 1</time>
                 <div className="text-lg font-black">Vraag een schouwing aan</div>
                 Vrijblijvend formulier invullen, nadat u van ons een telefoontje heeft ontvangen voor het maken van een afspraak voor de inspectie. Ons uiterst professionele personeel zal de hele procedure uitleggen, al uw vragen beantwoorden en een geschikt tijdstip voor de inspectie afspreken om te zien wat er mogelijk is om op uw dak te installeren.<br></br><br></br>
                 
