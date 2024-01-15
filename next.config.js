1ede17fa1b300e3e65c3b339a19ca5c1c06387ac
besoll <b.kavzharadze@gmail.com>
2024-01-15 13:59:48 +0100

    add npm i @ducanh2912/next-pwa in order to have robot.txt for schouwing

--- next.config.js
+++ next.config.js
@@ -1,35 +1,34 @@
-// const withPWA = require("@ducanh2912/next-pwa").default({
-//     dest: "public",
-//     disable: process.env.NODE_ENV === "production",
-//     cacheOnFrontEndNavCaching: true,
-//     aggressiveFrontEndNavCaching: true,
-//     reloadOnOnline: true,
-//     swcMinify: true,
-//     workboxOptions: {
-//         disableDevLogs: true,
-//       },
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+    disable: process.env.NODE_ENV === "production",
+    cacheOnFrontEndNavCaching: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    workboxOptions: {
+        disableDevLogs: true,
+      },
 
-//   });
-
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     images: {
-//         domains: ['cdn.sanity.io']
-//     },
-//     optimizeFonts: true,
-// }
-
-// module.exports = withPWA(nextConfig)
+  });
 
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['cdn.sanity.io'],
+        domains: ['cdn.sanity.io']
     },
-};
+}
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig)
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         domains: ['cdn.sanity.io'],
+//     },
+// };
+
+// module.exports = nextConfig;
 
 
