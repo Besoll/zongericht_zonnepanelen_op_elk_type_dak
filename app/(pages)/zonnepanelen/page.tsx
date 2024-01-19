6231f5d3789b1ed77e19129bc7eb99f4af3d3809
besoll <b.kavzharadze@gmail.com>
2024-01-19 17:23:58 +0100

    update logo and icons

--- app/(pages)/zonnepanelen/page.tsx
+++ app/(pages)/zonnepanelen/page.tsx
@@ -2,10 +2,23 @@ import React from 'react'
 
 const page = () => {
   return (
-    <main className='min-h-[70vh] flex justify-center items-center '>
-      <h1 className='text-black heading4 text-center'>
-        404 | pagina
-      </h1>
+    <main className='flex-center mx-auto w-full flex-col text-white snap-y'>
+      <header className='w-full  flex-center gradient_special_Hero2 flex h-[100vh] xs:h-[95vh]'>        
+        
+      </header>
+
+      <section className='-mt-10 xs:-mt-20'>
+         
+      </section>
+
+      <section className='max-w-screen-3xl my-8 md:my-10  scroll-mt-28 snap-start' id='timelineSection' >
+        
+      </section>
+
+      <section className='flex justify-center items-center w-full gradient_green min-h-full flex-row scroll-mt-12 snap-start'  id="zohoForm">
+       
+      </section>
+
     </main>
   )
 }
