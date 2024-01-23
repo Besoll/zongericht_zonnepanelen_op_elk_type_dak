b787cc123937cdf0be01040996d213d7fe42158e
besoll <b.kavzharadze@gmail.com>
2024-01-23 12:29:17 +0100

    zonnepanelen hero section

--- app/(pages)/zonnepanelen/page.tsx
+++ app/(pages)/zonnepanelen/page.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import Hero from '@/components/zonnepanelen/Hero'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const page = () => {
   return (
-    <main className='flex-center mx-auto w-full flex-col text-white snap-y'>
-      <header className='w-full  flex-center gradient_special_Hero2 flex h-[100vh] xs:h-[95vh]'>        
-        
+    <main className='flex-center mx-auto w-full flex-col text-white snap-y gap-2'>
+      <header className='w-full  flex-center gradient_special_Hero2 flex h-[100vh] xs:h-[95vh]'>
+        <Hero />
       </header>
 
       <section className='-mt-10 xs:-mt-20'>
-         
+          <Link href='#timelineSection'>
+            <Image 
+                  src='/icons/arrowDown.svg'
+                  alt='arrow down svg'
+                  width={80}
+                  height={80}
+                  className="object-cover text-white-400 w-[50px] xs:w-[80px] h-auto px-2 animate-bounce animate-duration-[1600ms] animate-ease-linear"
+              />               
+          </Link>
       </section>
 
       <section className='max-w-screen-3xl my-8 md:my-10  scroll-mt-28 snap-start' id='timelineSection' >
---
+++ components/zonnepanelen/Hero.tsx
@@ -0,0 +1,82 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Hero = () => {
+  return (
+    <div className='w-full h-full flex justify-center items-center flex-col md:flex-row px-8 md:px-20 py-8 md:py-20 max-w-[1750px]'>
+        <div className='w-full flex flex-col text-center md:text-start justify-center md:justify-start items-center md:items-start mb-6 md:mb-0'>
+            <h1 className='heading3 mb-3 mt-8  text-white pr-5 uppercase
+                        xs:heading2 
+                        xl:heading1'>
+                <span className=''>
+                    Glas op Glas
+                </span> 
+                <span 
+                    className='flex py-2 px-2 -ml-2 scale-70
+                                 border-2 border-l-brand_original_secondary border-r-brand_original_secondary
+                                text-orange w-fit rounded-3xl
+                                md:hover:scale-105 md:transition md:duration-300 md:ease-in-out md:cursor-pointer'>
+                        zonnepanelen <br className='flex md:hidden'></br> geproduceerd
+                </span> 
+                <span className=''>
+                    in Duitsland
+                </span>
+            </h1>
+
+            <div className='md:heading4 flex-col md:flex-row mb-4'>
+                <span className='py-2 px-2 -ml-2 
+                                text-gradient_special md:text-gradient_special2 md:mr-4'>
+                    Hoger rendement
+                </span>
+                <br className='flex md:hidden'></br>
+                <span className='py-2 px-2 -ml-2
+                                text-gradient_special md:text-gradient_special2'>
+                    Betere bescherming van cellen
+                </span>
+            </div>
+            <p className='hidden md:flex max-w-[1200px] md:pr-12'>
+                Ons team van experts staat klaar om uw overgang naar duurzame energie te ondersteunen met onze hoogwaardige glas-glas zonnepanelen. Vul het onderstaande formulier in voor een gratis inspectie. We zullen uw wensen beoordelen, de ideale oplossing berekenen en een concurrerende prijs bieden voor de installatie. In slechts 2 weken kunt u beginnen met het opwekken van duurzame energie met onze betrouwbare service en een indrukwekkende garantie van 30 jaar.
+            </p>
+
+
+        </div>
+
+
+        <div className='w-full md:w-[45%] '>
+        <div className='lg:flex-col justify-start w-full  '>
+            <div className='flex flex-row justify-between pb-2 max-h-[500px]'>
+                <Image 
+                    src='/zonnepanelen/IQ-7X-small.webp'
+                    alt='glas glas zonnepanelen op elk type dak testimonial'
+                    width={140}
+                    height={125}
+                    className=" w-[50%] h-auto pr-2 md:hover:scale-105 md:transition md:duration-300 md:ease-in-out md:cursor-pointer"
+                />   
+                <Image 
+                    src='/zonnepanelen/MeyerBurger-M-small.webp'
+                    alt='glas glas zonnepanelen op elk type dak op dakkapel'
+                    width={140}
+                    height={125}
+                    className="w-[50%]  h-auto md:hover:scale-105 md:transition md:duration-300 md:ease-in-out md:cursor-pointer "
+                />   
+            </div>
+            <div className='hidden md:flex flex-row-reverse justify-between pb-2'>
+                <Image 
+                    src='/zongericht-logo.svg'
+                    alt='glas glas zonnepanelen op elk type dak testimonial'
+                    width={140}
+                    height={125}
+                    className="object-cover h-auto w-[100%] md:hover:scale-105 md:transition md:duration-300 md:ease-in-out md:cursor-pointer "
+                />  
+            </div>   
+        </div>
+
+
+        </div>
+        
+          
+    </div>   
+  )
+}
+
+export default Hero
