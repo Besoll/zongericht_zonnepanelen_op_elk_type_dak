92290917b402c9a6d4df0e3ff5ef6c78d343848e
besoll <b.kavzharadze@gmail.com>
2024-03-19 16:06:05 +0100

    found issue folder name, change it and fix testimonialBIG photos

--- components/__lp/001/ui/SwiperBig.tsx
+++ components/__lp/001/ui/SwiperBig.tsx
@@ -13,34 +13,34 @@ import { Navigation, Pagination, EffectCoverflow, Mousewheel, Keyboard, Controll
 
 import dynamic from 'next/dynamic'
 const Stars5 = dynamic(() => import("@/components/__lp/001/icons/Stars5"))
-// const SwiperButtonNext = dynamic(() => import("@/components/__lp/001/icons/SwiperButtonNext"))
-// const SwiperButtonPrev = dynamic(() => import("@/components/__lp/001/icons/SwiperButtonPrev"))
+const SwiperButtonNext = dynamic(() => import("@/components/__lp/001/icons/SwiperButtonNext"))
+const SwiperButtonPrev = dynamic(() => import("@/components/__lp/001/icons/SwiperButtonPrev"))
 
 
 
-// import image1_big from "@/public/lp/reviews/big/1-1.webp"
-// import image1_normal from "@/public/lp/reviews/normal/1-1.webp"
-// import image1_small from "@/public/lp/reviews/small/1-1.webp"
+import image1_big from "@/public/lp/Reviews/big/1-1.webp"
+import image1_normal from "@/public/lp/Reviews/normal/1-1.webp"
+import image1_small from "@/public/lp/Reviews/small/1-1.webp"
 
-// import image2_big from "@/public/lp/reviews/big/2-1.webp"
-// import image2_normal from "@/public/lp/reviews/normal/2-1.webp"
-// import image2_small from "@/public/lp/reviews/small/2-1.webp"
+import image2_big from "@/public/lp/Reviews/big/2-1.webp"
+import image2_normal from "@/public/lp/Reviews/normal/2-1.webp"
+import image2_small from "@/public/lp/Reviews/small/2-1.webp"
 
-// import image3_big from "@/public/lp/reviews/big/3-1.webp"
-// import image3_normal from "@/public/lp/reviews/normal/3-1.webp"
-// import image3_small from "@/public/lp/reviews/small/3-1.webp"
+import image3_big from "@/public/lp/Reviews/big/3-1.webp"
+import image3_normal from "@/public/lp/Reviews/normal/3-1.webp"
+import image3_small from "@/public/lp/Reviews/small/3-1.webp"
 
-// import image4_big from "@/public/lp/reviews/big/4-1.webp"
-// import image4_normal from "@/public/lp/reviews/normal/4-1.webp"
-// import image4_small from "@/public/lp/reviews/small/4-1.webp"
+import image4_big from "@/public/lp/Reviews/big/4-1.webp"
+import image4_normal from "@/public/lp/Reviews/normal/4-1.webp"
+import image4_small from "@/public/lp/Reviews/small/4-1.webp"
 
-// import image5_big from "@/public/lp/reviews/big/5-1.webp"
-// import image5_normal from "@/public/lp/reviews/normal/5-1.webp"
-// import image5_small from "@/public/lp/reviews/small/5-1.webp"
+import image5_big from "@/public/lp/Reviews/big/5-1.webp"
+import image5_normal from "@/public/lp/Reviews/normal/5-1.webp"
+import image5_small from "@/public/lp/Reviews/small/5-1.webp"
 
 
-// // import daveCover from "@/public/lp/reviews/daveCover.webp"
-// import { TestimonialBigData } from '@/components/__lp/constants/TestimonialBigData';
+import daveCover from "@/public/lp/Reviews/daveCover.webp"
+import { TestimonialBigData } from '@/components/__lp/constants/TestimonialBigData';
 
 
 
@@ -97,24 +97,26 @@ const Stars5 = dynamic(() => import("@/components/__lp/001/icons/Stars5"))
         >
           <SwiperSlide>
             <div className='relative mb-8'>
-              <Image src='/lp/reviews/big/1-1.webp' alt="Alt text" width={510} height={700} 
+              <Image src={image1_big} alt="Alt text" width={510} height={700} 
                 className='hidden xl:flex justify-center items-center w-[85%] min-w-[300px] md:w-full max-w-[450px] h-auto object-cover rounded-3xl'/>
-              <Image src='/lp/reviews/normal/1-1.webp' alt="Alt text" width={510} height={700} 
+              <Image src={image1_normal} alt="Alt text" width={510} height={700} 
                 className='hidden md:flex xl:hidden justify-center items-center w-[85%] min-w-[300px] md:w-full max-w-[450px] h-auto object-cover rounded-3xl'/>
-              <Image src='/lp/reviews/small/1-1.webp' alt="Alt text" width={510} height={700} 
+              <Image src={image1_small} alt="Alt text" width={510} height={700} 
                 className='md:hidden flex justify-center items-center w-[85%] min-w-[300px] md:w-full max-w-[450px] h-auto object-cover rounded-3xl'/>
 
               <div className="hidden hover:flex  top-0 left-0 bg-black-100 justify-center items-center w-[85%] min-w-[300px] md:w-full max-w-[450px] h-auto object-cover rounded-3xl">
                   <Image 
-                      src='/lp/reviews/daveCover'
-                      alt='M.Tilborg, Zwijndrecht - cover'
-                      width={510}
-                      height={700}
-                      className="w-full h-auto"
-                  />
+                        src={daveCover}
+                        alt='M.Tilborg, Zwijndrecht - cover'
+                        width={510}
+                        height={700}
+                        className="w-full h-auto"
+                    />
                 <div className="show_content">
                   <Stars5 />
-                  <p className="card_brief">"Al met al kan ik zeggen dat mijn ervaring met Zongericht positief is. Van begin tot eind overtroffen ze mijn verwachtingen met hun toewijding aan kwaliteit, snelle service, expertise en vriendelijkheid."</p>
+                  <p className="card_brief">"Al met al kan ik zeggen dat mijn ervaring met Zongericht positief is. Van begin
+                    tot eind overtroffen ze mijn verwachtingen met hun toewijding aan kwaliteit, snelle service, expertise
+                    en vriendelijkheid."</p>
                   <h4 className="card_author">M.Tilborg, Zwijndrecht</h4>
                   <p className="card_info">Huiseigenaar</p>
                 </div>
@@ -122,35 +124,35 @@ const Stars5 = dynamic(() => import("@/components/__lp/001/icons/Stars5"))
             </div>
           </SwiperSlide>
           <SwiperSlide>
-            <Image src='/lp/reviews/big/2-1.webp' alt="Alt text" width={510} height={700} 
+            <Image src={image2_big} alt="Alt text" width={510} height={700} 
               className='hidden xl:flex justify-center items-center w-[85%] min-w-[300px] md:w-full max-w-[450px] h-auto object-cover rounded-3xl mb-2'/>
-            <Image src='/lp/reviews/normal/2-1.webp' alt="Alt text" width={510} height={700} 
+            <Image src={image2_normal} alt="Alt text" width={510} height={700} 
               className='hidden md:flex xl:hidden justify-center items-center w-[85%] min-w-[300px] md:w-full max-w-[450px] h-auto object-cover rounded-3xl mb-2'/>
-            <Image src='/lp/reviews/small/2-1.webp' alt="Alt text" width={510} height={700} 
+            <Image src={image2_small} alt="Alt text" width={510} height={700} 
               className='md:hidden flex justify-center items-center w-[85%] min-w-[300px] md:w-full max-w-[450px] h-auto object-cover rounded-3xl mb-2'/>
           </SwiperSlide>
           <SwiperSlide>
-            <Image src='/lp/reviews/big/3-1.webp' alt="Alt text" width={510} height={700} 
+            <Image src={image3_big} alt="Alt text" width={510} height={700} 
               className='hidden xl:flex justify-center items-center w-[85%] min-w-[300px] md:w-full max-w-[450px] h-auto object-cover rounded-3xl mb-2'/>
-            <Image src='/lp/reviews/normal/3-1.webp' alt="Alt text" width={510} height={700} 
+            <Image src={image3_normal} alt="Alt text" width={510} height={700} 
               className='hidden md:flex xl:hidden justify-center items-center w-[85%] min-w-[300px] md:w-full max-w-[450px] h-auto object-cover rounded-3xl mb-2'/>
-            <Image src='/lp/reviews/small/3-1.webp' alt="Alt text" width={510} height={700} 
+            <Image src={image3_small} alt="Alt text" width={510} height={700} 
               className='md:hidden flex justify-center items-center w-[85%] min-w-[300px] md:w-full max-w-[450px] h-auto object-cover rounded-3xl mb-2'/>
           </SwiperSlide>
           <SwiperSlide>
-            <Image src='/lp/reviews/big/4-1.webp' alt="Alt text" width={510} height={700} 
+            <Image src={image4_big} alt="Alt text" width={510} height={700} 
               className='hidden xl:flex justify-center items-center w-[85%] min-w-[300px] md:w-full max-w-[450px] h-auto object-cover rounded-3xl mb-2'/>
-            <Image src='/lp/reviews/normal/4-1.webp' alt="Alt text" width={510} height={700} 
+            <Image src={image4_normal} alt="Alt text" width={510} height={700} 
               className='hidden md:flex xl:hidden justify-center items-center w-[85%] min-w-[300px] md:w-full max-w-[450px] h-auto object-cover rounded-3xl mb-2'/>
-            <Image src='/lp/reviews/small/4-1.webp' alt="Alt text" width={510} height={700} 
+            <Image src={image4_small} alt="Alt text" width={510} height={700} 
               className='md:hidden flex justify-center items-center w-[85%] min-w-[300px] md:w-full max-w-[450px] h-auto object-cover rounded-3xl mb-2'/>
           </SwiperSlide>
           <SwiperSlide>
-            <Image src='/lp/reviews/big/5-1.webp' alt="Alt text" width={510} height={700} 
+            <Image src={image5_big} alt="Alt text" width={510} height={700} 
               className='hidden xl:flex justify-center items-center w-[85%] min-w-[300px] md:w-full max-w-[450px] h-auto object-cover rounded-3xl mb-2'/>
-            <Image src='/lp/reviews/normal/5-1.webp' alt="Alt text" width={400} height={600} 
+            <Image src={image5_normal} alt="Alt text" width={400} height={600} 
               className='hidden md:flex xl:hidden justify-center items-center w-[85%] min-w-[300px] md:w-full max-w-[450px] h-auto object-cover rounded-3xl mb-2'/>
-            <Image src='/lp/reviews/small/5-1.webp' alt="Alt text" width={260} height={350} 
+            <Image src={image5_small} alt="Alt text" width={260} height={350} 
               className='md:hidden flex justify-center items-center w-[85%] min-w-[300px] md:w-full max-w-[450px] h-auto object-cover rounded-3xl mb-2'/>
           </SwiperSlide>
         </Swiper>
