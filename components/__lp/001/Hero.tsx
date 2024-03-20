af08f0b41e75e197cadef13821f07e4cff321a74
besoll <b.kavzharadze@gmail.com>
2024-03-20 11:30:38 +0100

    fixe lazy img

--- components/__lp/001/Hero.tsx
+++ components/__lp/001/Hero.tsx
@@ -30,27 +30,33 @@ const Hero = () => {
       
         <div className='flex flex-col relative justify-center items-center w-[35%] md:min-w-[300px] lg:min-w-[450px]'>
             <div className='w-full h-auto min-w-[300px] min-h-[412px] md:min-h-[400px] lg:min-h-[600px] rounded-[500px]'>
-                <img
-                    src='/lp/001/1-big.webp'
-                    alt='zonnepanelen op schuin pvc dak'
-                    width={510}
-                    height={700}                    
-                    className="w-full h-auto min-w-[300px] rounded-[500px] xl:flex hidden"
-                />
-                <img 
-                    src='/lp/001/1-small.webp'
-                    alt='zonnepanelen op schuin pvc dak'
-                    width={510}
-                    height={700}                    
-                    className="w-full h-auto min-w-[300px] rounded-[500px] md:flex xl:hidden hidden"
-                />
-                <img 
-                    src='/lp/001/1-small.webp'
-                    alt='zonnepanelen op schuin pvc dak'
-                    width={510}
-                    height={700}
-                    className="w-full h-auto min-w-[300px] rounded-[500px] md:hidden flex"
-                />            
+                <div className="w-full h-auto min-w-[300px] rounded-[500px] xl:flex hidden">
+                    <img
+                        src='/lp/001/1-big.webp'
+                        alt='zonnepanelen op schuin pvc dak'
+                        width={510}
+                        height={700}                    
+                        className="w-full h-auto min-w-[300px] rounded-[500px] xl:flex hidden"
+                    />
+                </div>
+                <div className="w-full h-auto min-w-[300px] rounded-[500px] md:flex xl:hidden hidden">
+                    <img 
+                        src='/lp/001/1-small.webp'
+                        alt='zonnepanelen op schuin pvc dak'
+                        width={510}
+                        height={700}                    
+                        className="w-full h-auto min-w-[300px] rounded-[500px] md:flex xl:hidden hidden"
+                    />
+                </div>
+                <div className="w-full h-auto min-w-[300px] rounded-[500px] md:hidden flex">
+                    <img 
+                        src='/lp/001/1-small.webp'
+                        alt='zonnepanelen op schuin pvc dak'
+                        width={510}
+                        height={700}
+                        className="w-full h-auto min-w-[300px] rounded-[500px] md:hidden flex"
+                    />           
+                </div>
             </div>
             <div className='bg-white w-[95%] h-auto py-4 px-4 rounded-lg shadow-md flex flex-row gap-2 items-center justify-center absolute bottom-2 md:hover:scale-105 md:hover:skew-y-2  md:transition md:duration-300 md:ease-in-out md:cursor-pointer md:hover:rotate-2
             min-w-[300px]
@@ -58,7 +64,7 @@ const Hero = () => {
                 <Image 
                     src='/lp/001/1-2.webp'
                     alt='zonnepanelen op schuin pvc dak'
-                    width={480}
+                    width={120}
                     height={480}
                     className="w-[50%] lg:w-[55%] h-auto"
                 />
