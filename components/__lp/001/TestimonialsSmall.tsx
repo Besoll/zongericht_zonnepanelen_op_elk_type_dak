490f59e1e227438ccaa3fbc72eb37b8712a3ba94
besoll <b.kavzharadze@gmail.com>
2024-03-22 13:50:31 +0100

    deployed also seccond and improved swiper for testimonials saml, with database and also navigation buttons imporved

--- components/__lp/001/TestimonialsSmall.tsx
+++ components/__lp/001/TestimonialsSmall.tsx
@@ -6,7 +6,7 @@ const SwiperSmall = dynamic(() => import("@/components/__lp/001/ui/SwiperSmall")
 
 const TestimonialsSmall = () => {
   return (
-    <div className='flex flex-col justify-center items-center px-4 max-w-[1440px] gap-6 py-12 md:py-16'>
+    <div className='w-full flex flex-col justify-center items-center px-4 max-w-[1440px] gap-6 py-12 md:py-16'>
       <h2 className='heading3 md:heading2 font-semibold text-center'>
         Verhalen van tevreden klanten
       </h2>
